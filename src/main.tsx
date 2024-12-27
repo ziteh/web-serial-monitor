06a8fc8395b58d32ca85990919042aad4bf0d678
ziteh <honmonoh@gmail.com>
2024-12-27 21:32:18 +0800

    feat: add basic sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Layout from "./layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </StrictMode>,
 );
