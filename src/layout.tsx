01273d183aab0f3d794489aa8fdf47f777fdbbe8
ziteh <honmonoh@gmail.com>
2024-12-28 00:00:02 +0800

    feat: add dark theme

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,14 +1,17 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main className="flex-1 flex flex-col h-full w-full">
-        {/* <SidebarTrigger /> */}
-        {children}
-      </main>
-    </SidebarProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex-1 flex flex-col h-full w-full">
+          {/* <SidebarTrigger /> */}
+          {children}
+        </main>
+      </SidebarProvider>
+    </ThemeProvider>
   );
 }
