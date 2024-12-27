2d3e9c19f20a6b2bf967f9a9a05c0dfb0b304f55
ziteh <honmonoh@gmail.com>
2024-12-27 22:52:41 +0800

    feat: add terminal

---
+++ src/components/terminal.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Terminal() {
+  return (
+    <div className="flex flex-col h-screen gap-2 p-4">
+      <Textarea
+        readOnly
+        placeholder="Received message"
+        className="w-full flex-grow h-4/5 resize-none"
+      />
+      <div className="flex w-full gap-2 h-1/5">
+        <Textarea
+          placeholder="Send message"
+          className="flex-grow resize-none"
+        />
+        <Button className=" h-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-send-2"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z" />
+            <path d="M6.5 12h14.5" />
+          </svg>
+          {/* Send */}
+        </Button>
+      </div>
+    </div>
+  );
+}
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,12 +1,12 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
+      <main className="flex-1 flex flex-col h-full w-full">
+        {/* <SidebarTrigger /> */}
         {children}
       </main>
     </SidebarProvider>
