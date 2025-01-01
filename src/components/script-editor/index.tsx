c6b7122543ed5f5de395453032f3f404d66eb977
ziteh <honmonoh@gmail.com>
2025-01-01 12:20:40 +0800

    feat: add user script dialog

--- src/components/script-editor/index.tsx
+++ src/components/script-editor/index.tsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-
-interface UserScript {
-  title: string;
-  func: (raw: Uint8Array) => Uint8Array;
-}
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export default function ScriptEditor() {
+  const [testInput, setTestInput] = useState("");
+  const [testOutput, setTestOutput] = useState("");
   const [script, setScript] = useState(`
-    if (raw[0] === 0x01) {
-      raw[0] = 0x0F;
+if (raw[0] === 0x41) {
+  raw[0] = 0x42;
 }
-      return raw;`);
+return raw;
+`);
 
   const handleSaveScript = async () => {
     const userScript = new Function("raw", script);
@@ -27,8 +32,8 @@ export default function ScriptEditor() {
 
   const handleRunScript = async () => {
     const userScript = new Function("raw", script);
-    const processedData = userScript(new Uint8Array([0x01, 0x02, 0x03]));
-    console.log(processedData);
+    const processedData = userScript(new TextEncoder().encode(testInput));
+    setTestOutput(new TextDecoder().decode(processedData));
   };
 
   useEffect(() => {
@@ -40,14 +45,28 @@ export default function ScriptEditor() {
 
   return (
     <>
-      <Button onClick={handleSaveScript}>Save</Button>
-      <Textarea
-        value={script}
-        onChange={(e) => setScript(e.target.value)}
-        className="terminal"
-      />
-      <Input placeholder="Title" value="A123" />
-      <Button onClick={handleRunScript}>Run</Button>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Open</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>User Script Editor</DialogTitle>
+          </DialogHeader>
+          <Textarea
+            value={script}
+            onChange={(e) => setScript(e.target.value)}
+            className="terminal"
+          />
+          <Input
+            placeholder="Input"
+            onChange={(e) => setTestInput(e.target.value)}
+          />
+          <Input placeholder="Output" value={testOutput} readOnly />
+          <Button onClick={handleRunScript}>Test</Button>
+          <Button onClick={handleSaveScript}>Save</Button>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
